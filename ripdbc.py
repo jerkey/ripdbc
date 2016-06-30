980c041d5e5a389a37e0824facc0fec9de73bc8d
jake waters <jerkey@g-cipher.net>
2016-06-29 17:59:02 -0700

    ignore descriptors beginning with lowercase and followed by long strings

--- ripdbc.py
+++ ripdbc.py
@@ -18,11 +18,11 @@ def main():
           inFile.close()
           break
       spl = line.split()
-      if ((len(spl) == 4) and (spl[0]=='DCD') and (spl[2]==';')):
-          descr = spl[3] # name of CAN data
+      if ((len(spl) == 4) and (spl[0]=='DCD') and (spl[2]==';') and (ord(spl[3][1]) & 32 == 0)):
+          descr = spl[3] # name of CAN data              spl[3] not begin with lowercase ^^^
           line = inFile.readline()
           spl = line.split()
-          if ((len(spl) == 2) and (spl[0]=='DCD')):
+          if ((len(spl) == 2) and (spl[0]=='DCD') and (len(spl[1]) >= 5) and (len(spl[1]) <= 6)):
               canid = spl[1]
               print(canid+'\t'+descr)
 

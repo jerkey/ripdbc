e51dd887ba1c3c80057a59dd1cf3ea0c08f78b79
jake waters <jerkey@g-cipher.net>
2016-06-29 19:12:24 -0700

    now it works to create a .dbc file for SavvyCAN

--- ripdbc.py
+++ ripdbc.py
@@ -17,12 +17,13 @@ def main():
       line = getLine(inFile)
       spl = line.split()
       if ((len(spl) == 4) and (spl[0]=='DCD') and (spl[2]==';') and (ord(spl[3][1]) & 32 == 0)):
-          descr = spl[3] # name of CAN data              spl[3] not begin with lowercase ^^^
+          descr = spl[3][1:-1] # strip "" from name of CANid       not begin with lowercase ^^^
           line = getLine(inFile)
           spl = line.split()
           if ((len(spl) == 2) and (spl[0]=='DCD') and (len(spl[1]) >= 5) and (len(spl[1]) <= 6)):
               canid = spl[1]
-              print(canid+'\t'+descr)
+              canid_dec = int(canid,16) #convert from hex string to integer
+              print("BO_ "+str(canid_dec)+' '+descr+': 0 Vector__XXX')
 
 if __name__ == "__main__":
   main()

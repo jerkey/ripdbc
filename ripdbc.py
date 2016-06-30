79737627c6c60d8fcf1b1cf587df2afa0604deb3
jake waters <jerkey@g-cipher.net>
2016-06-29 18:41:28 -0700

    introduce getLine() and fix BDY_messages hunt line

--- ripdbc.py
+++ ripdbc.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
 import os,sys
 
+def getLine(file):
+    line = file.readline()
+    if not line:
+        file.close()
+        sys.exit()
+    return line
+
 def main():
   inFile = open(sys.argv[1],'r')
-  line = inFile.readline()
-  print(1)
-  while line.find('CANBusList')==-1:
-      line = inFile.readline()
-  print(2)
-  while line.find('BDY_messages')==-1:
-      line = inFile.readline()
-
-  print(3)
+  line = getLine(inFile)
+  while not (line.find('BDY_messages')==0):
+      line = getLine(inFile)
   while True:
-      line = inFile.readline()
-      if not line:
-          inFile.close()
-          break
+      line = getLine(inFile)
       spl = line.split()
       if ((len(spl) == 4) and (spl[0]=='DCD') and (spl[2]==';') and (ord(spl[3][1]) & 32 == 0)):
           descr = spl[3] # name of CAN data              spl[3] not begin with lowercase ^^^
-          line = inFile.readline()
+          line = getLine(inFile)
           spl = line.split()
           if ((len(spl) == 2) and (spl[0]=='DCD') and (len(spl[1]) >= 5) and (len(spl[1]) <= 6)):
               canid = spl[1]

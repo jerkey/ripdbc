3214a4564bbab5b8e258a3f88b3cedb3b53b0c6a
jake waters <jerkey@g-cipher.net>
2017-02-12 23:04:46 -0800

    dbcdest.py now actually works right

--- dbcdest.py
+++ dbcdest.py
@@ -13,18 +13,19 @@ def main():
   dest = sys.argv[2] # CAN destination we are looking for
   line = getLine(inFile)
   while True:
-      while not (line.find('BO_')==0): # seek to the beginning of a CAN ID def
+      while not (line.find('BO_ ')==0): # seek to the beginning of a CAN ID def
           line = getLine(inFile)
       spl = line.split()
-      canid_int = int(spl[1],10) # convert from dec string to integer
+      canid_dec = spl[1]
+      canid_int = int(canid_dec,10) # convert from dec string to integer
       canid_hex = hex(canid_int) # convert from int to hex string '0x18'
       canid_name = spl[2] # 'GTW_12VStatus'
       canid_source = spl[4] # 'GTW'
-      while len(line) > 0:
+      while len(line) > 2:
           line = getLine(inFile)
           spl = line.split()
           if len(spl) > 0 and spl[-1].find(dest)>=0 and canid_source != dest: # look for dest in listeners
-              print(canid_hex+"\t"+canid_name+"\tfrom "+canid_source) # output the CAN ID description and origin
+              print(canid_hex+"\t"+canid_name.ljust(20)+"from "+canid_source) # output the CAN ID description and origin
               line = '' # make len(line) zero so this while loop exits
 
 if __name__ == "__main__":
